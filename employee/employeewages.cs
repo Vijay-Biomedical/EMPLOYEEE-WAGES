c025569288cd76e068a690fc14f37af4efaa35e5
unknown <vijayofficial580@gmail.com>
2022-04-01 08:20:49 +0530

    first commit

---
+++ employee/employeewages.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EMPLOYEE_WAGES
+    {
+        class Employee
+        {
+            static void Main(string[] args)
+            {
+                int isPresent = 1;
+                Random random = new Random();
+                int Empinput = random.Next(0, 2);
+                if (isPresent == Empinput)
+                    Console.WriteLine("EMPLOYEE IS PRESENT");
+                else
+                    Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+    }
+
+
