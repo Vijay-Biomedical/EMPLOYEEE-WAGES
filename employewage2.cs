adc07ccd3e6e9d98fc5f8c2cdf1802fc384dd875
Thiru <vijayofficial580@gmail.com>
2022-04-01 09:27:42 +0530

    commit again

---
+++ employewage2.cs
@@ -0,0 +1,40 @@
+﻿
+        
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+
+namespace EMPLOYEE_WAGES
+    {
+        class Employee
+        {
+            static void Main(string[] args)
+            {
+                int isPresent = 1;
+                int EMP_WAGE_PER_HR = 20, EMP_HR = 8, empwage = 0;
+                Random random = new Random();
+                int Empinput = random.Next(0, 2);
+            if (isPresent == Empinput)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+                EMP_HR = 8;
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+                EMP_HR = 0;
+            }
+                empwage = EMP_HR * EMP_WAGE_PER_HR;
+                Console.WriteLine("EMPLOYEE WAGE :" + empwage);
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+
+
+
